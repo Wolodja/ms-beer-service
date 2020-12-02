014417beae75de24b62793ceeafdbcb6a7ce3dd6
Wolodja <wolodjawaskovych@i.ua>
2020-12-02 11:31:09 +0100

    Updating UPC. Closes #16

--- src/main/java/guru/springframework/msbeerservice/bootstrap/BeerLoader.java
+++ src/main/java/guru/springframework/msbeerservice/bootstrap/BeerLoader.java
@@ -10,6 +10,10 @@ import java.math.BigDecimal;
 @Component
 public class BeerLoader implements CommandLineRunner {
 
+    public static String BEER_1_UPC = "0631234200036";
+    public static String BEER_2_UPC = "0631234300019";
+    public static String BEER_3_UPC = "0083783375213";
+
     private final BeerRepository beerRepository;
 
     public BeerLoader(BeerRepository beerRepository) {
@@ -28,7 +32,7 @@ public class BeerLoader implements CommandLineRunner {
                 .beerStyle("IPA")
                 .quantityToBrew(200)
                 .minOnHand(12)
-                .upc(23423432423L)
+                .upc(BEER_1_UPC)
                 .price(new BigDecimal("12.95"))
                 .build());
 
@@ -37,8 +41,17 @@ public class BeerLoader implements CommandLineRunner {
                 .beerStyle("PALE_ALE")
                 .quantityToBrew(200)
                 .minOnHand(12)
-                .upc(20013432423L)
+                .upc(BEER_2_UPC)
                 .price(new BigDecimal("11.95"))
+                .build());
+
+            beerRepository.save(Beer.builder()
+                .beerName("No Hammers On the Bar")
+                .beerStyle("PALE_ALE")
+                .quantityToBrew(200)
+                .minOnHand(12)
+                .upc(BEER_3_UPC)
+                .price(new BigDecimal("10.95"))
                 .build());
         }
     }
--- src/test/java/guru/springframework/msbeerservice/web/controller/BeerControllerTest.java
+++ src/test/java/guru/springframework/msbeerservice/web/controller/BeerControllerTest.java
@@ -1,17 +1,22 @@
 package guru.springframework.msbeerservice.web.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import guru.springframework.msbeerservice.bootstrap.BeerLoader;
+import guru.springframework.msbeerservice.services.BeerService;
 import guru.springframework.msbeerservice.web.model.BeerDto;
 import guru.springframework.msbeerservice.web.model.BeerStyleEnum;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.math.BigDecimal;
 import java.util.UUID;
 
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -24,8 +29,13 @@ class BeerControllerTest {
     @Autowired
     ObjectMapper objectMapper;
 
+    @MockBean
+    BeerService beerService;
+
     @Test
     void getBeerById() throws Exception {
+
+        given(beerService.getById(any())).willReturn(getValidBeerDto());
 
         mockMvc.perform(get("/api/v1/beer/" + UUID.randomUUID().toString())
             .accept(MediaType.APPLICATION_JSON))
@@ -38,6 +48,8 @@ class BeerControllerTest {
         BeerDto beerDto = getValidBeerDto();
         String beerDtoJson = objectMapper.writeValueAsString(beerDto);
 
+        given(beerService.saveNewBeer(any())).willReturn(getValidBeerDto());
+
         mockMvc.perform(post("/api/v1/beer/")
             .contentType(MediaType.APPLICATION_JSON)
             .content(beerDtoJson))
@@ -48,6 +60,8 @@ class BeerControllerTest {
     void updateBeer() throws Exception {
         BeerDto beerDto = getValidBeerDto();
         String beerDtoJson = objectMapper.writeValueAsString(beerDto);
+
+        given(beerService.updateBeer(any(), any())).willReturn(getValidBeerDto());
 
         mockMvc.perform(put("/api/v1/beer/" + UUID.randomUUID())
             .contentType(MediaType.APPLICATION_JSON)
@@ -60,7 +74,7 @@ class BeerControllerTest {
             .beerName("My Beer")
             .beerStyle(BeerStyleEnum.ALE)
             .price(new BigDecimal("2.99"))
-            .upc(13213213L)
+            .upc(BeerLoader.BEER_1_UPC)
             .build();
     }
 }
