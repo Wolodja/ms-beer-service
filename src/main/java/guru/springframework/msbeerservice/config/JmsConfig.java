31b55b21b96e5f658a3e406c6e8c5ba5316b0639
Wolodja <wolodjawaskovych@i.ua>
2021-01-30 15:18:58 +0100

    Configured JMS. Closes #31

---
+++ src/main/java/guru/springframework/msbeerservice/config/JmsConfig.java
@@ -0,0 +1,20 @@
+package guru.springframework.msbeerservice.config;
+
+import org.springframework.jms.support.converter.MappingJackson2MessageConverter;
+import org.springframework.jms.support.converter.MessageConverter;
+import org.springframework.jms.support.converter.MessageType;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+
+@Configuration
+public class JmsConfig {
+
+    @Bean
+    public MessageConverter jacksonJmsMessageConverter() {
+        MappingJackson2MessageConverter converter = new MappingJackson2MessageConverter();
+        converter.setTargetType(MessageType.TEXT);
+        converter.setTypeIdPropertyName("_type");
+        return converter;
+    }
+}
